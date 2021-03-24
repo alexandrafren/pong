2f2dc418d43266171322b58fc73b4591eabb8b04
alexandrafren <alex.fren@datadoghq.com>
2021-03-23 22:50:52 -0400

    game fully functions

--- pong.py
+++ pong.py
@@ -6,6 +6,9 @@ gm.bgcolor("lightblue")
 gm.setup(width=800, height=600)
 gm.tracer(0)
 
+score_a = 0
+score_b = 0
+
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -30,6 +33,14 @@ ball.penup()
 ball.goto(0,0)
 ball.dx = 2
 ball.dy = 2
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -71,10 +82,18 @@ while True:
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-    if ball.xcor() > 390 or ball.xcor() < -390:
+    if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
-
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
