88d6cfc66436f42988010af5e82c816729b91a59
alexandrafren <alex.fren@datadoghq.com>
2021-03-23 21:51:55 -0400

    functions for paddle movements

--- pong.py
+++ pong.py
@@ -29,6 +29,33 @@ ball.color("white")
 ball.penup()
 ball.goto(0,0)
 
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+gm.listen()
+gm.onkeypress(paddle_a_up, "w")
+gm.onkeypress(paddle_a_down, "s")
+gm.onkeypress(paddle_b_up, "Up")
+gm.onkeypress(paddle_b_down, "Down")
+
+
 #main
 while True:
     gm.update()
